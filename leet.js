12f28dde57b6de860a7b47ea9718dacf6fb7a1c8
ssivaji-gb <sathirabathisivajir@gmail.com>
2025-12-05 17:38:13 +0530

    first commit

--- leet.js
+++ leet.js
@@ -1,13 +1,50 @@
-function twoSum(nums, target) {
-  for (i = 0; i < nums.length; i++) {
-    for (j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
-        return [i, j];
-      }
-    }
-  }
-}
-let s = [2, 7, 11, 15];
-let t = 9;
-let sum = twoSum(s, t);
-console.log(sum);
+// 1. Two Sum================
+
+// function twoSum(nums, target) {
+//   for (i = 0; i < nums.length; i++) {
+//     for (j = i + 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] === target) {
+//         return [i, j];
+//       }
+//     }
+//   }
+// }
+// let s = [2, 7, 11, 15];
+// let t = 9;
+// let sum = twoSum(s, t);
+// console.log(sum);
+
+
+
+// 2. Add Two Numbers===================
+
+// function addTwoNumbers(lone,ltwo){
+
+// let a=lone.reverse().toString().split(",").join("")
+// let b=ltwo.reverse().toString().split(",").join("")
+// let c= Number(a)+Number(b)
+// let rev=String(c).split('').map(Number).reverse()
+// return rev
+// }
+// let l1=[2,4,3]
+// let  l2 = [5,6,4]
+// console.log(addTwoNumbers(l1,l2))
+
+// 3.strictly-palindromic-number=============
+
+// function isPalindrome (x) {
+
+// let b=x.toString().split("").reverse().join("")
+// let c= parseInt(b)
+// if(x==c){
+//     return true
+// }
+// return false
+// };
+// console.log(isPalindrome(12));
+
+ 
+
+
+
+
